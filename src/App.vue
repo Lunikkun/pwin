a38bf55ce6916f89caa4742edc00771a86ac718e
Antonio <antoniosica0710@gmail.com>
2025-03-26 18:24:55 +0100

    speed Insights added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import SiteNavbar from './components/SiteNavbar.vue';
 import FrameComponent from './components/FrameComponent.vue'
+import SpeedInsights  from "@vercel/speed-insights/next"
 </script>
 
 <template>
   <SiteNavbar />
   <FrameComponent />
+  <SpeedInsights />
 </template>
