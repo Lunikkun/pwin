56e415b6672c563065c946fabf5a70b819402ef5
Antonio <antoniosica0710@gmail.com>
2025-03-26 18:25:51 +0100

    per vue non per next

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import SiteNavbar from './components/SiteNavbar.vue';
 import FrameComponent from './components/FrameComponent.vue'
-import SpeedInsights  from "@vercel/speed-insights/next"
+import SpeedInsights  from "@vercel/speed-insights/vue"
 </script>
 
 <template>
