61bc104616687342113db5afb9503845b84567f4
Lunikk <antoniosica0710@gmail.com>
2025-03-26 18:59:38 +0100

    inject added for analytics

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import SiteNavbar from './components/SiteNavbar.vue';
 import FrameComponent from './components/FrameComponent.vue'
 import { SpeedInsights } from "@vercel/speed-insights/vue"
+import { inject } from "@vercel/analytics"
 </script>
 
 <template>
   <SiteNavbar />
   <FrameComponent />
   <SpeedInsights />
+  <inject/>
 </template>
