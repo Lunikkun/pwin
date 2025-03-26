695eee2f8f86a43a44f284c6355f0829b445b7e7
Antonio <antoniosica0710@gmail.com>
2025-03-26 17:52:37 +0100

    iframe e responsive layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import SiteNavbar from './components/SiteNavbar.vue';
-
+import FrameComponent from './components/FrameComponent.vue'
 </script>
 
 <template>
   <SiteNavbar />
+  <FrameComponent />
 </template>
---
+++ src/components/FrameComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-screen h-screen">
+        <iframe
+            src="https://view.publitas.com/prowin-winter-gmbh/sr_magazin_1-hbj_2025_dina4_it_it/page/1"
+            class="w-full h-full border-none"
+            title="Google"
+            sandbox="allow-same-origin allow-scripts allow-popups"
+        ></iframe>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "FrameComponent",
+};
+</script>
